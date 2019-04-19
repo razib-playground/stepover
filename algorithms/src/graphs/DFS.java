4cb42d4a70237427b598189c63175773b073d13e
razib <razib.fh@gmail.com>
2019-04-20 00:45:14 +0600

    adding resources directory

--- algorithms/src/graphs/DFS.java
+++ algorithms/src/graphs/DFS.java
@@ -1,8 +1,13 @@
 package graphs;
 
+import java.io.*;
+
 public class DFS {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws FileNotFoundException{
+        System.setIn(new FileInputStream(new File("./resources/DFS.in.txt")));
+        System.setOut(new PrintStream(new File("./resources/DFS.out.txt")));
+
+        System.out.println("Hellow World");
     }
 }
