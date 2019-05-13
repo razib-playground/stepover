4f34ef7957ca0fd876326bf783380a407eca3f12
razibfh <razib.fh@gmail.com>
2019-05-14 02:06:57 +0600

    Dijkstra implementation with printPath

--- algorithms/src/graphs/DijkstraV1.java
+++ algorithms/src/graphs/DijkstraV1.java
@@ -5,6 +5,7 @@ package graphs;
 import java.io.*;
 import java.util.*;
 
+@SuppressWarnings("Duplicates")
 public class DijkstraV1 {
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -23,15 +24,70 @@ public class DijkstraV1 {
 
             for (int i = 0; i < nEdges; i++) {
                 int u = scanner.nextInt() - 1;
-                int v = scanner.nextInt() -1 ;
+                int v = scanner.nextInt() - 1;
                 int w = scanner.nextInt();
 
-
+                if (costs[u][v] == 0 || (costs[u][v] != 0 && costs[u][v] > w)) costs[u][v] = w;
+                costs[v][u] = costs[u][v];
             }
+            int source = scanner.nextInt() - 1;
+
+            //dijkstra start
+            int[] distances = new int[nNodes];
+            boolean[] visited = new boolean[nNodes];
+            int[] parents = new int[nNodes];
+            Arrays.fill(distances, Integer.MAX_VALUE);
+
+            distances[source] = 0;
+            parents[source] = -1;
+            for (int i = 0; i < nNodes - 1; i++) {
+
+                //find the unvisited vertex with minimum distance
+                int index = 0;
+                int min = Integer.MAX_VALUE;
+                for (int j = 0; j < nNodes; j++) {
+                    if (!visited[j] && distances[j] < min) {
+                        index = j;
+                        min = distances[j];
+                    }
+                }
+
+                //relaxation: updating the connected unvisited node distances based on the new min dastances
+                visited[index] = true;
+                for (int j = 0; j < nNodes; j++) {
+                    if (!visited[j] && costs[index][j] != 0 && distances[index] != Integer.MAX_VALUE
+                            && distances[j] > (distances[index] + costs[index][j])) {
+                        distances[j] = distances[index] + costs[index][j];
+                        parents[j] = index;
+                    }
+                }
+            }
+            //dijkstra end
+
+            //printing the distances from source
+            for (int i = 0; i < nNodes; i++) {
+                if (i != source) {
+                    if (distances[i] == Integer.MAX_VALUE) System.out.print("-1 ");
+                    else System.out.print(distances[i] + " ");
+                }
+            }
+            System.out.println();
+
+            //printing the parent of each node
+            //System.out.println(Arrays.toString(parents));
+            printPath(parents, nNodes-1);
+            System.out.println("\n.........x.........x.........x.........x.........x.........\n");
+
         }
 
         System.setIn(new FileInputStream(FileDescriptor.in));
         System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
         scanner.close();
+    }
+
+    private static void printPath(int[] parents, int index) {
+        if (parents[index] == -1) return;
+        printPath(parents, parents[index]);
+        System.out.print(parents[index] + 1 +" ");
     }
 }
